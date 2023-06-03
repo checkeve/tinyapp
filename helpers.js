4f480b465aaef6f02c5fb52f233072510f78702a
checkeve <joanna.check@gmail.com>
2023-06-03 13:31:03 -0700

    Added testing of getUserByEmail function with mocha and chai.

---
+++ helpers.js
@@ -0,0 +1,12 @@
+//function to find object of individual user's information inside users object
+const getUserByEmail = function(email, database) {
+  for (const userId in database) {
+    const user = database[userId];
+    if (user.email === email) {
+      return user;
+    }
+  }
+  return null;
+};
+
+module.exports = {getUserByEmail};
